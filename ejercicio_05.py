cee4c38145e7d9cd99ff19abcf718adf09c19df0
JonathanReyesAltamirano27 <reyesaltamiranojonathan71@gmail.com>
2026-01-23 10:42:44 -0600

    Sube ejercicio 5

---
+++ ejercicio_05.py
@@ -0,0 +1,8 @@
+# Convertir grados a Fahrenheit a grados Celsius
+
+Fahrenheit = float(input("Ingresa los grados: "))
+
+Celsius =(fahremheit -32) * (5 / 9)
+
+print("Fahrenheit", fahrenheit)
+print("Celsius", Celsius)
